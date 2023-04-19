ea976b4b2e728a7cc8940fdf62a4e8fd671f1c2d
Kriti-be21 <109240831+Kriti-be21@users.noreply.github.com>
2023-04-19 22:51:09 +0530

    Update strem.py

--- strem.py
+++ strem.py
@@ -1,1 +1,56 @@
+import streamlit as st
+import pandas as pd
+import pickle
 
+st.write("""
+# Finding largest no App
+""")
+#Get Input
+
+st.header('User Input Parameters')
+
+def user_input_features():
+    Number1 = st.number_input("NUMBER 1")
+    Number2 = st.number_input("NUMBER 2")
+    Number3 = st.number_input("NUMBER 3")
+    
+
+    data = {'NUMBER1': Number1,
+            'NUMBER2': Number2,
+            'NUMBER3': Number3,
+            }
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+
+#Preprocessing
+
+continous_features = ['NUMBER1','NUMBER2','NUMBER3']
+font_size=30
+font_color="blue"
+st.subheader('The Largest Number is :')
+if(df['NUMBER1'][0]>df['NUMBER2'][0] and df['NUMBER1'][0]>df['NUMBER3'][0]):
+    html_str = f"""<style>p.a {{color: {font_color};font: bold {font_size}px Courier;}}</style><p class="a">{df['NUMBER1'][0]}</p>"""
+    st.markdown(html_str, unsafe_allow_html=True)
+if(df['NUMBER2'][0]>df['NUMBER1'][0] and df['NUMBER2'][0]>df['NUMBER3'][0]):
+    html_str = f"""<style>p.a {{color: {font_color};font: bold {font_size}px Courier;}}</style><p class="a">{df['NUMBER2'][0]}</p>"""
+    st.markdown(html_str, unsafe_allow_html=True)
+if(df['NUMBER3'][0]>df['NUMBER2'][0] and df['NUMBER3'][0]>df['NUMBER1'][0]):
+    html_str = f"""<style>p.a {{color: {font_color};font: bold {font_size}px Courier;}}</style><p class="a">{df['NUMBER3'][0]}</p>"""
+    st.markdown(html_str, unsafe_allow_html=True)
+    
+if(df['NUMBER1'][0]==df['NUMBER2'][0] and df['NUMBER1'][0]>df['NUMBER3'][0]):
+    html_str = f"""<style>p.a {{color: {font_color};font: bold {font_size}px Courier;}}</style><p class="a">{df['NUMBER1'][0]}</p>"""
+    st.markdown(html_str, unsafe_allow_html=True)
+
+elif(df['NUMBER1'][0]==df['NUMBER3'][0] and df['NUMBER1'][0]>df['NUMBER2'][0]):
+    html_str = f"""<style>p.a {{color: {font_color};font: bold {font_size}px Courier;}}</style><p class="a">{df['NUMBER1'][0]}</p>"""
+    st.markdown(html_str, unsafe_allow_html=True)
+elif(df['NUMBER2'][0]==df['NUMBER3'][0] and df['NUMBER1'][0]<df['NUMBER2'][0]):
+    html_str = f"""<style>p.a {{color: {font_color};font: bold {font_size}px Courier;}}</style><p class="a">{df['NUMBER2'][0]}</p>"""
+    st.markdown(html_str, unsafe_allow_html=True)
+elif(df['NUMBER1'][0]==df['NUMBER3'][0] and df['NUMBER1'][0]==df['NUMBER2'][0]):
+    html_str = f"""<style>p.a {{color: {font_color};font: bold {font_size}px Courier;}}</style><p class="a">{df['NUMBER1'][0]}</p>"""
+    st.markdown(html_str, unsafe_allow_html=True)
